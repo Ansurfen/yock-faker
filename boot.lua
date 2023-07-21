cfe4a248f664f317cbffd4b395909ec18d9ac7cf
ansurfen <axf593161@gmail.com>
2023-07-21 14:13:15 +0800

    release 1.0.2

--- boot.lua
+++ boot.lua
@@ -4,16 +4,7 @@ return {
     name = "faker",
     version = defaultVersion,
     load = function(opt)
-        local file = multi_fetch(env.platform:Zip(), {
-            Target = wrapzip("faker-" .. defaultVersion),
-            Version = opt.ver or defaultVersion
-        }, {
-            github = "https://github.com/ansurfen/yock-faker/releases/download/{{.Version}}/{{.Target}}",
-            gitee = "https://gitee.com/ansurfen/yock-faker/releases/download/{{.Version}}/{{.Target}}"
-        })
-        uncompress(file, pathf(env.yock_path, "yock_modules"))
     end,
-    unload = function()
-        rm({ safe = false }, pathf(env.yock_path, "yock_modules/faker"))
+    unload = function(opt)
     end
 }
